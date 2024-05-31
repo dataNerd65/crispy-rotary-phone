b646efb76146c76e884f25fcabdc75f61bafe4db
dataNerd65 <pkiarie811@gmail.com>
2024-05-31 22:20:47 +0300

    Setting up the project with even the dependencies eg javafx

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+
+
+
